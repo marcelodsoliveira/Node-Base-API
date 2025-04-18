85a65b76b9022cca01396fe642053e16518a6484
Marcelo Santos <oliveira.lnx@gmail.com>
2025-04-18 17:33:11 -0300

    Added auth verification

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../models/User';
+
 
 export const Auth = {
-  private: (req: Request, res: Response, next: NextFunction) => {
+  private: async (req: Request, res: Response, next: NextFunction) => {
     // Fazer verificação de auth
     let success = false;
+
+    // Fazendo verificação de auth
+    if ( req.headers.authorization ) {
+      let hash:string = req.headers.authorization.substring(6);
+      let decoded = Buffer.from(hash, 'base64').toString();
+      let data:string[] = decoded.split(':');
+
+      // Verificação para saber se o usuário tentando login tem um usuário e senha no database
+      if(data.length == 2){
+        let hasUser = await User.findOne({
+          where: {
+            email: data[0],
+            password: data[1]
+          }
+        });
+        if(hasUser) {
+          success = true;
+        }
+      }      
+    }
 
     if(success) {
       next();
